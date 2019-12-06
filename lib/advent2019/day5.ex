798a555e237aa0cb1896f19b04ccfb53f40511a0
Melinda Weathers <melindaweathers@gmail.com>
2019-12-05 20:52:52 -0500

    Move IntCode into its own module
    * Probably going to regret this

--- lib/advent2019/day5.ex
+++ lib/advent2019/day5.ex
@@ -1,135 +1,14 @@
 #mix run -e 'Day5.run'
 defmodule Day5 do
-  defmodule Program do
-    defstruct instructions: Map.new(), pos: 0, inputs: [], outputs: [], halt: false
-  end
-
-  def run_program(str, inputs \\ []) do
-    map = parse_instructions(str)
-    _run_program(%Program{instructions: map, pos: 0, inputs: inputs, outputs: []})
-  end
-  defp _run_program(%Program{halt: true} = program) do
-    outputs = program.outputs |> Enum.reverse
-    %{ program | outputs: outputs }
-  end
-  defp _run_program(program) do
-    { op, param_modes } = parse_op(program.instructions[program.pos])
-    run_op(program, op, param_modes) |> _run_program
-  end
-
-  def parse_op(op) do
-    { rem(op, 100),
-      %{1 => rem(div(op,100),10),
-        2 => rem(div(op,1000),10),
-        3 => rem(div(op,10000),10),
-        4 => rem(div(op,100000),10) } }
-  end
-
-  def lookup(program, offset, param_modes), do: _lookup(program, offset, param_modes[offset])
-  def _lookup(program, offset, 0), do: program.instructions[program.instructions[program.pos + offset]]
-  def _lookup(program, offset, 1), do: program.instructions[program.pos + offset]
-
-  def set_val(program, offset, val) do
-    set_pos = program.instructions[program.pos + offset]
-    new_map = Map.put(program.instructions, set_pos, val)
-    %{ program | instructions: new_map }
-  end
-
-  def advance(program, offset), do: %{ program | pos: program.pos + offset }
-
-  def set_inputs(program, inputs), do: %{ program | inputs: inputs }
-
-  # Op 99 - Halt
-  def run_op(program, 99, _), do: %{ program | halt: true }
-
-  # Op 1 - Add
-  def run_op(program, 1, param_modes) do
-    add1 = lookup(program, 1, param_modes)
-    add2 = lookup(program, 2, param_modes)
-    set_val(program, 3, add1 + add2) |> advance(4)
-  end
-
-  # Op 2 - Mult
-  def run_op(program, 2, param_modes) do
-    add1 = lookup(program, 1, param_modes)
-    add2 = lookup(program, 2, param_modes)
-    set_val(program, 3, add1 * add2) |> advance(4)
-  end
-
-  # Op 3 - Read Input
-  def run_op(program, 3, _param_modes) do
-    [this_input | other_inputs] = program.inputs
-    set_val(program, 1, this_input) |> set_inputs(other_inputs) |> advance(2)
-  end
-
-  # Op 4 - Output
-  def run_op(program, 4, param_modes) do
-    output = lookup(program, 1, param_modes)
-    %{ program | outputs: [output|program.outputs] } |> advance(2)
-  end
-
-  # Op 5 - Jump if true
-  def run_op(program, 5, param_modes) do
-    check_for_true = lookup(program, 1, param_modes)
-    if check_for_true != 0 do
-      new_pos = lookup(program, 2, param_modes)
-      %{ program | pos: new_pos }
-    else
-      program |> advance(3)
-    end
-  end
-
-  # Op 6 - Jump if false
-  def run_op(program, 6, param_modes) do
-    check_for_false = lookup(program, 1, param_modes)
-    if check_for_false == 0 do
-      new_pos = lookup(program, 2, param_modes)
-      %{ program | pos: new_pos }
-    else
-      program |> advance(3)
-    end
-  end
-
-  # Op 7 - less than
-  def run_op(program, 7, param_modes) do
-    param1 = lookup(program, 1, param_modes)
-    param2 = lookup(program, 2, param_modes)
-    val = if param1 < param2, do: 1, else: 0
-    set_val(program, 3, val) |> advance(4)
-  end
-
-  # Op 8 - equals
-  def run_op(program, 8, param_modes) do
-    param1 = lookup(program, 1, param_modes)
-    param2 = lookup(program, 2, param_modes)
-    val = if param1 == param2, do: 1, else: 0
-    set_val(program, 3, val) |> advance(4)
-  end
-
-  def parse_instructions(str) do
-    str
-    |> String.trim
-    |> String.split(",")
-    |> Enum.with_index(0)
-    |> Enum.map(fn {k,v}->{v, String.to_integer(k)} end)
-    |> Map.new
-  end
-
-  def get_instructions(program) do
-    map = program.instructions
-    indices = Map.keys(map) |> Enum.sort
-    Enum.map(indices, fn(i) ->
-      Map.get(map, i)
-    end) |> Enum.join(",")
-  end
+  require IntCode
 
   def run do
     {:ok, operations} = File.read('inputs/day5.txt')
 
     IO.puts "First Star"
-    IO.inspect run_program(operations, [1]).outputs
+    IO.inspect IntCode.run_program(operations, [1]).outputs
 
     IO.puts "Second Star"
-    IO.inspect run_program(operations, [5]).outputs
+    IO.inspect IntCode.run_program(operations, [5]).outputs
   end
 end
---
+++ test/int_code_test.exs
@@ -0,0 +1,66 @@
+defmodule IntCodeTest do
+  use ExUnit.Case, async: true
+
+  test "halt immediately" do
+    assert IntCode.run_program("99") |> IntCode.get_instructions == "99"
+  end
+
+  test "simple tests" do
+    assert IntCode.run_program("1,0,0,0,99") |> IntCode.get_instructions == "2,0,0,0,99"
+    assert IntCode.run_program("1,0,0,0,99") |> IntCode.get_instructions == "2,0,0,0,99"
+    assert IntCode.run_program("1,9,10,3,2,3,11,0,99,30,40,50") |> IntCode.get_instructions == "3500,9,10,70,2,3,11,0,99,30,40,50"
+    assert IntCode.run_program("2,3,0,3,99") |> IntCode.get_instructions == "2,3,0,6,99"
+    assert IntCode.run_program("2,4,4,5,99,0") |> IntCode.get_instructions == "2,4,4,5,99,9801"
+    assert IntCode.run_program("1,1,1,4,99,5,6,0,99") |> IntCode.get_instructions == "30,1,1,4,2,5,6,0,99"
+  end
+
+  test "inputs and outputs" do
+    program = IntCode.run_program("3,0,4,0,99", [123])
+    assert program.outputs == [123]
+    assert IntCode.get_instructions(program) == "123,0,4,0,99"
+  end
+
+  test "param modes" do
+    assert IntCode.run_program("1002,4,3,4,33") |> IntCode.get_instructions == "1002,4,3,4,99"
+  end
+
+  test "negative values" do
+    assert IntCode.run_program("1101,100,-1,4,0") |> IntCode.get_instructions == "1101,100,-1,4,99"
+  end
+
+  test "equals and less than" do
+    # Test input is equal to 8
+    assert IntCode.run_program("3,9,8,9,10,9,4,9,99,-1,8", [8]).outputs == [1]
+    assert IntCode.run_program("3,9,8,9,10,9,4,9,99,-1,8", [2]).outputs == [0]
+
+    # Test input is less than 8
+    assert IntCode.run_program("3,9,7,9,10,9,4,9,99,-1,8", [7]).outputs == [1]
+    assert IntCode.run_program("3,9,7,9,10,9,4,9,99,-1,8", [8]).outputs == [0]
+    assert IntCode.run_program("3,9,7,9,10,9,4,9,99,-1,8", [9]).outputs == [0]
+
+    # Test input is equal to 8
+    assert IntCode.run_program("3,3,1108,-1,8,3,4,3,99", [8]).outputs == [1]
+    assert IntCode.run_program("3,3,1108,-1,8,3,4,3,99", [2]).outputs == [0]
+
+    # Test input is less than 8
+    assert IntCode.run_program("3,3,1107,-1,8,3,4,3,99", [2]).outputs == [1]
+    assert IntCode.run_program("3,3,1107,-1,8,3,4,3,99", [8]).outputs == [0]
+    assert IntCode.run_program("3,3,1107,-1,8,3,4,3,99", [9]).outputs == [0]
+  end
+
+  test "jumps" do
+    assert IntCode.run_program("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9", [0]).outputs == [0]
+    assert IntCode.run_program("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9", [2]).outputs == [1]
+    assert IntCode.run_program("3,3,1105,-1,9,1101,0,0,12,4,12,99,1", [0]).outputs == [0]
+    assert IntCode.run_program("3,3,1105,-1,9,1101,0,0,12,4,12,99,1", [2]).outputs == [1]
+  end
+
+  test "larger example" do
+    prog1 = IntCode.run_program("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99", [7])
+    assert prog1.outputs == [999]
+    prog2 = IntCode.run_program("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99", [8])
+    assert prog2.outputs == [1000]
+    prog3 = IntCode.run_program("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99", [9])
+    assert prog3.outputs == [1001]
+  end
+end
