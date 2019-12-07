578e78a375d1461a872345732dfd4201b30734f5
Melinda Weathers <melindaweathers@gmail.com>
2019-12-06 22:35:51 -0500

    Day 6 part 2

--- lib/advent2019/day6.ex
+++ lib/advent2019/day6.ex
@@ -23,22 +23,33 @@ defmodule Day6 do
     _build_orbits(remaining, Map.put(universe, this_name, this_planet))
   end
 
-  def print_universe(universe) do
-    Enum.map(universe, fn({name, planet}) ->
-      IO.inspect name
-      IO.inspect planet
-    end)
-  end
   def count_orbits(universe) do
-    #print_universe(universe)
     Enum.map(universe, fn({_name, planet}) ->
       count_orbits_for_planet(planet.parent_name, universe)
     end) |> Enum.sum
   end
+
   def count_orbits_for_planet(nil, _universe), do: 0
   def count_orbits_for_planet(planet_name, universe) do
     planet = universe[planet_name]
     1 + count_orbits_for_planet(planet.parent_name, universe)
+  end
+
+  def get_parents(planet_name, universe), do: _get_parents(universe[planet_name].parent_name, universe, [])
+  defp _get_parents(nil, _universe, parents), do: parents
+  defp _get_parents(planet_name, universe, parents) do
+    planet = universe[planet_name]
+    _get_parents(planet.parent_name, universe, [planet_name|parents])
+  end
+
+  def count_common_parents(from, to) do
+    MapSet.intersection(MapSet.new(from), MapSet.new(to)) |> MapSet.size
+  end
+
+  def count_transfers(from, to, universe) do
+    from_parents = get_parents(from, universe)
+    to_parents = get_parents(to, universe)
+    Enum.count(from_parents) + Enum.count(to_parents) - (2*count_common_parents(from_parents, to_parents))
   end
 
   def run do
@@ -49,5 +60,12 @@ defmodule Day6 do
     universe = load_file('inputs/day6.txt')
     IO.puts "First Star"
     IO.puts count_orbits(universe)
+
+    sample_universe2 = load_file('inputs/day6-sample2.txt')
+    IO.puts "Sample 2 (should be 4)"
+    IO.puts count_transfers("YOU","SAN",sample_universe2)
+
+    IO.puts "Second Star"
+    IO.puts count_transfers("YOU","SAN",universe)
   end
 end
