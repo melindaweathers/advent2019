5eba4dd9252312933af9c915565690a42f026e3d
Melinda Weathers <melindaweathers@gmail.com>
2019-12-07 15:03:16 -0500

    Slim down IntCode

--- lib/advent2019/int_code.ex
+++ lib/advent2019/int_code.ex
@@ -3,110 +3,20 @@ defmodule IntCode do
     defstruct instructions: Map.new(), pos: 0, inputs: [], outputs: [], halt: false, needs_input: false
   end
 
+  @op_map %{ 99 => :halt, 1 => :add, 2 => :mult, 3 => :input, 4 => :output, 5 => :jumpif, 6 => :jumpunless, 7 => :lt, 8 => :eq }
+
   def run_program(str, inputs \\ []) do
     map = parse_instructions(str)
     _run_program(%Program{instructions: map, pos: 0, inputs: inputs, outputs: []})
   end
-  defp _run_program(%Program{halt: true} = program), do: program
-  defp _run_program(%Program{needs_input: true} = program), do: program
-  defp _run_program(program) do
-    { op, param_modes } = parse_op(program.instructions[program.pos])
-    run_op(program, op, param_modes) |> _run_program
+  defp _run_program(%Program{halt: true} = prog), do: prog
+  defp _run_program(%Program{needs_input: true} = prog), do: prog
+  defp _run_program(prog) do
+    { op, modes } = parse_op(prog.instructions[prog.pos])
+    run_op(prog, @op_map[op], modes) |> _run_program
   end
 
-  def resume_with_inputs(program, inputs) do
-    _run_program( %{ program | needs_input: false, inputs: inputs } )
-  end
-
-  def parse_op(op) do
-    { rem(op, 100),
-      %{1 => rem(div(op,100),10),
-        2 => rem(div(op,1000),10),
-        3 => rem(div(op,10000),10),
-        4 => rem(div(op,100000),10) } }
-  end
-
-  def lookup(program, offset, param_modes), do: _lookup(program, offset, param_modes[offset])
-  def _lookup(program, offset, 0), do: program.instructions[program.instructions[program.pos + offset]]
-  def _lookup(program, offset, 1), do: program.instructions[program.pos + offset]
-
-  def set_val(program, offset, val) do
-    set_pos = program.instructions[program.pos + offset]
-    new_map = Map.put(program.instructions, set_pos, val)
-    %{ program | instructions: new_map }
-  end
-
-  def advance(program, offset), do: %{ program | pos: program.pos + offset }
-
-  def set_inputs(program, inputs), do: %{ program | inputs: inputs }
-
-  # Op 99 - Halt
-  def run_op(program, 99, _), do: %{ program | halt: true }
-
-  # Op 1 - Add
-  def run_op(program, 1, param_modes) do
-    add1 = lookup(program, 1, param_modes)
-    add2 = lookup(program, 2, param_modes)
-    set_val(program, 3, add1 + add2) |> advance(4)
-  end
-
-  # Op 2 - Mult
-  def run_op(program, 2, param_modes) do
-    add1 = lookup(program, 1, param_modes)
-    add2 = lookup(program, 2, param_modes)
-    set_val(program, 3, add1 * add2) |> advance(4)
-  end
-
-  # Op 3 - Read Input
-  def run_op(%Program{inputs: []} = program, 3, _param_modes), do: %{ program | needs_input: true }
-  def run_op(program, 3, _param_modes) do
-    [this_input | other_inputs] = program.inputs
-    set_val(program, 1, this_input) |> set_inputs(other_inputs) |> advance(2)
-  end
-
-  # Op 4 - Output
-  def run_op(program, 4, param_modes) do
-    output = lookup(program, 1, param_modes)
-    %{ program | outputs: program.outputs ++ [output] } |> advance(2)
-  end
-
-  # Op 5 - Jump if true
-  def run_op(program, 5, param_modes) do
-    check_for_true = lookup(program, 1, param_modes)
-    if check_for_true != 0 do
-      new_pos = lookup(program, 2, param_modes)
-      %{ program | pos: new_pos }
-    else
-      program |> advance(3)
-    end
-  end
-
-  # Op 6 - Jump if false
-  def run_op(program, 6, param_modes) do
-    check_for_false = lookup(program, 1, param_modes)
-    if check_for_false == 0 do
-      new_pos = lookup(program, 2, param_modes)
-      %{ program | pos: new_pos }
-    else
-      program |> advance(3)
-    end
-  end
-
-  # Op 7 - less than
-  def run_op(program, 7, param_modes) do
-    param1 = lookup(program, 1, param_modes)
-    param2 = lookup(program, 2, param_modes)
-    val = if param1 < param2, do: 1, else: 0
-    set_val(program, 3, val) |> advance(4)
-  end
-
-  # Op 8 - equals
-  def run_op(program, 8, param_modes) do
-    param1 = lookup(program, 1, param_modes)
-    param2 = lookup(program, 2, param_modes)
-    val = if param1 == param2, do: 1, else: 0
-    set_val(program, 3, val) |> advance(4)
-  end
+  def resume_with_inputs(prog, inputs), do: _run_program( %{ prog | needs_input: false, inputs: inputs } )
 
   def parse_instructions(str) do
     str
@@ -117,11 +27,68 @@ defmodule IntCode do
     |> Map.new
   end
 
-  def get_instructions(program) do
-    map = program.instructions
-    indices = Map.keys(map) |> Enum.sort
-    Enum.map(indices, fn(i) ->
-      Map.get(map, i)
-    end) |> Enum.join(",")
+  def get_instructions(prog) do
+    indices = Map.keys(prog.instructions) |> Enum.sort
+    (for i <- indices, do: prog.instructions[i]) |> Enum.join(",")
+  end
+
+  def parse_op(op) do
+    { rem(op, 100),
+      %{1 => rem(div(op,100),10),
+        2 => rem(div(op,1000),10),
+        3 => rem(div(op,10000),10),
+        4 => rem(div(op,100000),10) } }
+  end
+
+  def vget(prog, offset, modes), do: _vget(prog, offset, modes[offset])
+  def _vget(prog, offset, 0), do: prog.instructions[prog.instructions[prog.pos + offset]]
+  def _vget(prog, offset, 1), do: prog.instructions[prog.pos + offset]
+
+  def vset(prog, offset, val) do
+    set_pos = prog.instructions[prog.pos + offset]
+    new_map = Map.put(prog.instructions, set_pos, val)
+    %{ prog | instructions: new_map }
+  end
+
+  def jump_to(prog, pos), do: %{ prog | pos: pos }
+  def advance(prog, offset), do: jump_to(prog, prog.pos + offset)
+  def set_inputs(prog, inputs), do: %{ prog | inputs: inputs }
+
+  def run_op(prog, :halt, _), do: %{ prog | halt: true }
+
+  def run_op(prog, :add, modes) do
+    vset(prog, 3, vget(prog, 1, modes) + vget(prog, 2, modes)) |> advance(4)
+  end
+
+  def run_op(prog, :mult, modes) do
+    vset(prog, 3, vget(prog, 1, modes) * vget(prog, 2, modes)) |> advance(4)
+  end
+
+  def run_op(%Program{inputs: []} = prog, :input, _modes), do: %{ prog | needs_input: true }
+  def run_op(prog, :input, _modes) do
+    [this_input | other_inputs] = prog.inputs
+    vset(prog, 1, this_input) |> set_inputs(other_inputs) |> advance(2)
+  end
+
+  def run_op(prog, :output, modes) do
+    %{ prog | outputs: prog.outputs ++ [vget(prog, 1, modes)] } |> advance(2)
+  end
+
+  def run_op(prog, :jumpif, modes) do
+    if vget(prog, 1, modes) != 0, do: jump_to(prog, vget(prog, 2, modes)), else: advance(prog, 3)
+  end
+
+  def run_op(prog, :jumpunless, modes) do
+    if vget(prog, 1, modes) == 0, do: jump_to(prog, vget(prog, 2, modes)), else: advance(prog, 3)
+  end
+
+  def run_op(prog, :lt, modes) do
+    val = if vget(prog, 1, modes) < vget(prog, 2, modes), do: 1, else: 0
+    vset(prog, 3, val) |> advance(4)
+  end
+
+  def run_op(prog, :eq, modes) do
+    val = if vget(prog, 1, modes) == vget(prog, 2, modes), do: 1, else: 0
+    vset(prog, 3, val) |> advance(4)
   end
 end
